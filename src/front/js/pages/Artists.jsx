d5e02a529372994de2b2b245bae660920d3b7fb4
Andres Villani <73246809+AndresVillani00@users.noreply.github.com>
2025-03-08 09:24:14 +0000

    [Proyecto Final] correccion ventana artists

--- src/front/js/pages/Artists.jsx
+++ src/front/js/pages/Artists.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
-import { Link } from "react-router-dom";
 
 export const Artists = () => {
     const { store } = useContext(Context);
